f4d24367b3a762643144fa6e2bcb2362f8e7227d
Raj Karmakar <97rajmath@gmail.com>
2024-05-22 12:20:46 +0530

    Finished working on Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import './Header.css'
 
+import MonkLogo from '../../assets/svgs/monkLogo.svg'
+
 const Header = () => {
 	return (
 		<div className='headerContainer'>
-			Header
+			<img src={MonkLogo} alt='MonkLogo' className='monkLogo'/>
+			<h2 className='headerText'>Monk Upsell & Cross-Sell</h2>
 		</div>
 	)
 }
