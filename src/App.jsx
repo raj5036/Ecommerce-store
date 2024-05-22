c45781b01b1754274379ed76a60a19edad4ce1fc
Raj Karmakar <97rajmath@gmail.com>
2024-05-22 12:00:19 +0530

    Started with Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 
+import React from 'react'
+import Header from './components/Header/Header'
+
 function App() {
 
   return (
-    <>
-      Start
-    </>
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
   )
 }
 
