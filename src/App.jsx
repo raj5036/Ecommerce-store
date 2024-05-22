2dcecd307f94d00740ddb31a0d4b12ad9b59d4a8
Raj Karmakar <97rajmath@gmail.com>
2024-05-22 12:32:55 +0530

    Started with ProductList Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 
 import React from 'react'
 import Header from './components/Header/Header'
+import ProductList from './components/ProductList/ProductList'
 
 function App() {
 
   return (
     <React.Fragment>
       <Header />
+      <ProductList />
     </React.Fragment>
   )
 }
---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,11 @@
+import './ProductList.css'
+
+const ProductList = () => {
+	return (
+		<div className='productListContainer'>
+			productListContainer
+		</div>
+	)
+}
+
+export default ProductList
