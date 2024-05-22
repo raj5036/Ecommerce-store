e92213a2c77790348a68ad58e3c08ed7bfec206d
Raj Karmakar <97rajmath@gmail.com>
2024-05-22 13:11:44 +0530

    Added ProductList SubHeader

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,6 +4,10 @@ const ProductList = () => {
 	return (
 		<div className='productListContainer'>
 			<h1 className='listHeader'>Add products</h1>
+			<div className='listSubHeader'>
+				<div className='subHeaderItem'>Product</div>
+				<div className='subHeaderItem'>Discount</div>
+			</div>
 		</div>
 	)
 }
