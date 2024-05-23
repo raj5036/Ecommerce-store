ba56901bf6881b039ff4e3bc506b5b673749e26f
Raj Karmakar <97rajmath@gmail.com>
2024-05-23 17:52:25 +0530

    ProductPicker Modal basic styling finish

--- src/components/ProductPickerModal/ProductPickerModal.jsx
+++ src/components/ProductPickerModal/ProductPickerModal.jsx
@@ -28,24 +28,52 @@ const ProductPickerModal = ({ onCloseButtonClick }) => {
 						{allProducts.map((product, index) => {
 							return (
 								<div key={'product-list' + index} className='productContainer'>
-									{console.log(product)}
-									<input
-										type='checkbox'
-										className='checkbox'
-										name='checkbox'
-										onChange={e => console.log(e.target.checked)}
-									/>
-									<img 
-										src={product.image.src} 
-										alt={`product-image-${index}`}
-										className='productImage'
-									/>
-									<div>
-										{product.title}
+									<div className='mainProduct'>
+										<input
+											type='checkbox'
+											className='checkbox'
+											name='checkbox'
+											onChange={e => console.log(e.target.checked)}
+										/>
+										<img 
+											src={product.image.src} 
+											alt={`product-image-${index}`}
+											className='productImage'
+										/>
+										<div>
+											{product.title}
+										</div>
 									</div>
+									{product.variants.length && (<div className='variantsContainer'>
+										{product.variants.map((variant, index) => {
+											return (
+												<div key={'variant' + index} className='variant'>
+													<input
+														type='checkbox'
+														className='checkbox'
+														onChange={e => console.log(e.target.checked)}
+													/>
+													<div>
+														{variant.title}
+													</div>
+												</div>
+											)
+										})}
+									</div>)}
 								</div>
 							)
 						})}
+					</div>
+
+					<div className='modalFooter'>
+						<div>{'0 products selected'}</div>
+						<div className='buttons'>
+							<button className='button cancelButton'>Cancel</button>
+							<button 
+								className='button addButton'
+								disabled={true}
+							>Add</button>
+						</div>
 					</div>
 				</div>
 			</div>
