7a05b58b6df03e0782f04044dbeca34af019aa08
Raj Karmakar <97rajmath@gmail.com>
2024-05-23 16:28:12 +0530

    Added Modal Header and Modal Search bar

--- src/components/ProductPickerModal/ProductPickerModal.jsx
+++ src/components/ProductPickerModal/ProductPickerModal.jsx
@@ -1,12 +1,31 @@
-import { IoMdClose } from 'react-icons/io'
+import { useState } from 'react'
 import './ProductPickerModal.css'
 
+import { IoMdClose } from 'react-icons/io'
+import { useSelector } from 'react-redux'
+
 const ProductPickerModal = ({ onCloseButtonClick }) => {
+	const [searchInput, setSearchInput] = useState()
+
+	const allProducts = useSelector(state => state.allProducts.products)
+
 	return (
 		<div className='modal'>
 				<div className='modalContent'>
 					<IoMdClose className='close' onClick={onCloseButtonClick}/>
-					My content
+					<div className='modalHeader'>Add Products</div>
+					<input 
+						type="text" 
+						name="search" 
+						placeholder="Search.." 
+						className='searchInput'
+						value={searchInput}
+						onChange={e => setSearchInput(e.target.value)}
+					/>
+					
+					{allProducts.map((product, index) => {
+
+					})}
 				</div>
 			</div>
 	)
