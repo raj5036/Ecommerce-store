815bbcdcc43f56d3f9b032cb1e537918401240b4
Raj Karmakar <97rajmath@gmail.com>
2024-05-24 13:27:40 +0530

    Redux store logic update

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { Store, persistor } from './redux/Store.js'
-import { PersistGate } from 'redux-persist/integration/react'
+import { Store } from './redux/Store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={Store}>
-      <PersistGate loading={null} persistor={persistor}>
         <App />
-      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,20 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"
-import storage from 'redux-persist/lib/storage'
-import { persistReducer, persistStore } from 'redux-persist'
-import {thunk} from 'redux-thunk';
+import { persistStore } from 'redux-persist'
 import AllProductsReducer from './state/AllProductsSlice'
 import SelectedProductsReducer from "./state/SelectedProductsSlice";
 
-const persistConfig = {
-  key: 'root',
-  storage,
-}
-
 export const Store = configureStore({
 	reducer: {
-		selectedProducts: persistReducer(persistConfig, SelectedProductsReducer),
-		allProducts: persistReducer(persistConfig, AllProductsReducer),
-		middleware: [thunk]
+		selectedProducts: SelectedProductsReducer,
+		allProducts: AllProductsReducer,
 	}
 })
 
