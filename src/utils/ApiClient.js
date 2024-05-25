0855d5459e889bf04c0a568770c0a0d844dbd92b
Raj Karmakar <97rajmath@gmail.com>
2024-05-25 11:17:54 +0530

    Added x-api-key to API call

--- src/utils/ApiClient.js
+++ src/utils/ApiClient.js
@@ -3,7 +3,11 @@ import axios from "axios"
 export const getProducts = (searchItem, limit) => {
 	return Promise.resolve(
 		axios.get(
-			`http://stageapi.monkcommerce.app/task/products/search?search=${searchItem}&page=2&limit=${limit}`
+			`http://stageapi.monkcommerce.app/task/products/search?search=${searchItem}&page=2&limit=${limit}`, {
+				headers: {
+					'x-api-key': '72njgfa948d9aS7gs5'
+				}
+			}
 		)
 	)
 		.then(response => {
